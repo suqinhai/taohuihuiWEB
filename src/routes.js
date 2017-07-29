9c02cc83fa660fc1fc4a1fc41f184512adbc2413
suqinhai <467456744@qq.com>
2017-07-29 11:58:42 +0800

    修改

--- src/routes.js
+++ src/routes.js
@@ -9,6 +9,11 @@ const topNav = resolve => require(['./views/index/topNav.vue'], resolve)
 const bottomNav = resolve => require(['./views/index/bottomNav.vue'], resolve)
 const poster = resolve => require(['./views/index/poster.vue'], resolve)
 
+//商品管理模块
+const notOnline = resolve => require(['./views/goods/notOnline.vue'], resolve)
+const online = resolve => require(['./views/goods/online.vue'], resolve)
+
+
 let routes = [
     {
         path: '/login',
@@ -32,6 +37,17 @@ let routes = [
             { path: '/index/topNav', component: topNav, name: '头部菜单' },
             { path: '/index/bottomNav', component: bottomNav, name: '底部菜单' },
             { path: '/index/poster', component: poster, name: '海报轮播' },
+        ]
+    },
+    {
+        path: '/',
+        component: Home,
+        name: '商品管理',
+        iconCls: 'fa fa-address-card',
+        leaf: false,//有多个节点
+        children: [
+            { path: '/goods/notOnline', component: notOnline, name: '未上线商品' },
+            { path: '/goods/online', component: online, name: '已上线商品' },
         ]
     },
     {
